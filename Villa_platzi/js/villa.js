f8e7d399ba8c59b27b29ce6a4a72fe328ee84987
Jair Yara <jyara@oneago.com>
2019-10-25 23:09:14 -0500

    Draw background

--- Villa_platzi/js/villa.js
+++ Villa_platzi/js/villa.js
@@ -1,9 +1,26 @@
-var z;
+var vp = document.getElementById("villaPlatzi");
+var paper = vp.getContext("2d");
+var map = "tile.png";
 
-for (var i = 0; i < 10; i++) {
-    z = randomNumber(-100, 200);
-    document.write(z + ", ");
+var imageBackground = new Image();
+imageBackground.src = map;
+imageBackground.addEventListener("load", draw);
 
+var cow = new Image();
+cow.src = "vaca.png";
+cow.addEventListener("load", drawCow);
+
+var pig = new Image();
+pig.src = "cerdo.png";
+pig.addEventListener("load", drawPig);
+
+var chicken = new Image();
+chicken.src = "pollo.png";
+chicken.addEventListener("load", drawChicken);
+
+
+function draw() {
+    paper.drawImage(imageBackground, 0, 0);
 }
 
 
